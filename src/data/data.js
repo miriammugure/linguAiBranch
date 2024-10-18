4d2de9b9b57392dfc6f39f7bb45f5b60e898deaf
miriam <miriammugure610@gmail.com>
2024-10-18 21:57:52 +0300

    add the doctors section and the search section

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+export const patientsData = [
+    {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      email: "john.doe@example.com",
+      mobile: "123-456-7890",
+      name_of_kin: "Jane Doe",
+      phone_of_kin: "321-654-0987",
+    },
+    {
+      id: 2,
+      first_name: "Jane",
+      last_name: "Smith",
+      email: "jane.smith@example.com",
+      mobile: "987-654-3210",
+      name_of_kin: "John Smith",
+      phone_of_kin: "876-543-2109",
+    },
+    {
+      id: 3,
+      first_name: "Mike",
+      last_name: "Johnson",
+      email: "mike.johnson@example.com",
+      mobile: "555-123-4567",
+      name_of_kin: "Michelle Johnson",
+      phone_of_kin: "555-765-4321",
+    },
+  
+    {
+      id: 4,
+      first_name: "Alice",
+      last_name: "Brown",
+      email: "alice.brown@example.com",
+      mobile: "222-333-4444",
+      name_of_kin: "Bob Brown",
+      phone_of_kin: "444-555-6666",
+    },
+    {
+      id: 5,
+      first_name: "Robert",
+      last_name: "White",
+      email: "robert.white@example.com",
+      mobile: "333-444-5555",
+      name_of_kin: "Rebecca White",
+      phone_of_kin: "666-777-8888",
+    },
+  
+  ];
+  
--- src/page/Doctor.jsx
+++ src/page/Doctor.jsx
@@ -1,11 +1,78 @@
+import DataTable from 'react-data-table-component'
+import { patientsData } from "../data/data";
 
+const Doctors = () => {
+  
+  const columns = [
+    {
+      name: 'ID',
+      selector: row => row.id,
+      sortable: true,
+    },
+    {
+      name: 'First Name',
+      selector: row => row.first_name,
+      sortable: true,
+    },
+    {
+      name: 'Last Name',
+      selector: row => row.last_name,
+      sortable: true,
+    },
+    {
+      name: 'Email Address',
+      selector: row => row.email,
+      sortable: true,
+    },
+    {
+      name: 'Mobile Phone',
+      selector: row => row.mobile,
+      sortable: true,
+    },
+    {
+      name: 'Kin Name',
+      selector: row => row.name_of_kin,
+      sortable: true,
+    },
+     {
+      name: 'Kin Phone',
+      selector: row => row.phone_of_kin,
+      sortable: true,
+    },
+  ];
 
-function Doctor() {
   return (
-    <div>
-      
+    <div className="p-4">
+     <div className="flex justify-center items-center py-4">
+      <form className="w-full max-w-md">
+        <div className="flex items-center py-2">
+          <input
+className="appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none border-2 border-b-green-900"
+type="text"
+            placeholder="Search patients..."
+            aria-label="Search Patients"
+           
+          />
+          <button
+            className="flex-shrink-0 bg-green-900 hover:bg-green-600 border-none text-sm border-4 text-white py-1 px-2 rounded h-14 "
+            type="submit"
+          >
+            Search
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+      <h1 className="text-2xl font-bold mb-4">Patients List</h1>
+      <DataTable
+        columns={columns}
+        data={patientsData}
+        pagination
+        highlightOnHover
+        responsive
+        striped
+      />
+    </div>
+  );
+};
 
-export default Doctor
+export default Doctors;
